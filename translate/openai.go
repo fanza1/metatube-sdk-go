c14e6bb9491bd4d8bcfce94ad9fc61babd1f3e92
fanza1 <lichendi666@gmail.com>
2023-06-25 18:40:56 +0800

    add timeout and further change the error list

--- translate/openai.go
+++ translate/openai.go
@@ -10,13 +10,20 @@ import (
 	"strings"
 	"time"
 	"unicode"
+	"context"
 )
 
 var (
 	openaiAPI = "https://api.openai.com/v1/chat/completions"
 	error_list = []string{"AI", "不建议", "未成年", "18+", "成人内容", "18岁及以上", "提供其他帮助", "敏感内容", "成人主题", "OpenAI", "使用政策", 
 	"不当内容", "不能为其提供翻译", "随时告诉我", "为你提供帮助", "非常露骨", "成人影片信息", "不合法的行为", "不鼓励或支持", "公共场合观看", "不适合所有观众",
-	"符合规则", "描述性文本", 
+	"符合规则", "描述性文本", "遵守相关规定", "遵守规定", "相关规定", "不适合", "不适当", "冒犯性", "令人不安", "文明用语", "非常抱歉", "不合适", "不合法",
+	"很抱歉", "翻译内容", "翻译文本", "翻译文案", "无法翻译", "不支持", "不允许", "不符合", "不符合规定", "不符合规则", "不符合要求", "不符合条件", "不符合标准",
+	"使用条款", "条款", "社区准则", "准则", "违反", "违反规定", "违反规则", "违反要求", "违反条件", "违反标准", "违反政策", "违反法律", "违反法规", "违反法律法规",
+	"编程规则", "翻译", "进行翻译", "18+的影片", "谢谢您的理解", "规则", "标准", "政策", "法律", "法规", "法律法规", "描述性", "描述性内容",
+	"有争议", "敏感内容", "传播", "传播或引用", "注意", "这是一段", "成人影片", "语言模型", "偏袒", "冒犯", "请告诉我", "用户的程序", "翻译服务", "如果您",
+	"日本AV", "电影标题", "谨慎查看", "建议您", "标题", "中文是", "日本影片", "成人电影", "日本成人", "不适宜", "谨慎", "需要翻译",
+	"句子",
 	}
 )
 
@@ -43,9 +50,10 @@ func containsErrors(s string) bool {
 	errorsCount := 0
 	for _, err := range error_list {
 		if strings.Contains(s, err) {
+			fmt.Printf("Warning: %s matching %s\n", err, s)
 			errorsCount++
 		}
-		if errorsCount >= 2 {
+		if errorsCount >= 1 {
 			return true
 		}
 	}
@@ -75,8 +83,17 @@ func parse_text(text string) string {
 }
 
 func OpenaiTranslate(depth int, q, source, target, key string) (result string, err error) {
+
 	if depth == 4 {
-		return q, fmt.Errorf("Reached maximum recursion depth")
+		// q1, err1 := GoogleFreeTranslate(q, source, target)
+		// if err1 != nil {
+		// 	fmt.Println("Error: GoogleFreeTranslate:", err1)
+		// 	return q, err1
+		// }
+		// return q1, nil
+
+		fmt.Println("Error: Reached maximum depth:", q)
+		return q, nil
 	}
 
 	messages := []OpenaiMessages{
@@ -113,6 +130,11 @@ func OpenaiTranslate(depth int, q, source, target, key string) (result string, e
 		return "", err
 	}
 
+	// Create a context with a timeout of 30 seconds
+	limit := 30
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(limit)*time.Second)
+	defer cancel() // cancel when we are finished
+
 	req, err := http.NewRequest("POST", openaiAPI, bytes.NewBuffer(jsonBody))
 	if err != nil {
 		return "", err
@@ -122,10 +144,13 @@ func OpenaiTranslate(depth int, q, source, target, key string) (result string, e
 	req.Header.Set("Authorization", "Bearer "+key)
 
 	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		return "", err
-	}
+    resp, err := client.Do(req.WithContext(ctx))
+    // Check if the request was cancelled due to timeout
+    if err != nil {
+        fmt.Println("Error: Request cancelled due to timeout after", limit, "seconds")
+        return q, nil
+    }
+
 	defer resp.Body.Close()
 
 	body, _ := ioutil.ReadAll(resp.Body)
@@ -138,11 +163,13 @@ func OpenaiTranslate(depth int, q, source, target, key string) (result string, e
 	
 	for _, choice := range response.Choices {
 		if choice.Message.Role == "assistant" {
-			if containsErrors(choice.Message.Content) {
-				fmt.Printf("Debug: Error found in message content: %s\n", choice.Message.Content)
+			content := parse_text(choice.Message.Content)
+			if containsErrors(content) {
+				// fmt.Printf("Warning: errors found in message content: %s\n", content)
 				return OpenaiTranslate(depth+1, q, source, target, key)
 			}
-			return parse_text(choice.Message.Content), nil
+			fmt.Printf("Success: translated: %s\n", content)
+			return content, nil
 		}
 	}
 
